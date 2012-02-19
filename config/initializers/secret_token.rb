5894077445f9281daea87c78176acd56989b5780
Bill DePhillips <bill.dephillips@gmail.com>
2012-02-19 14:00:00 -0800

    initial add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pantry::Application.config.secret_token = 'aba81f8cf8ed77013552076c127e258d53983fa1331bc8e682534c2952aeee930a94950959ba3b0034084775ec4a94bc360cdcd5da9dd29031dd5ee71b9025d3'
